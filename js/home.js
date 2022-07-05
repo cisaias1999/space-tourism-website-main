b5a4d1f35fffd5772bd892de71abe0c371676fe6
Isaias Eduardo Castañeda Diaz <cisaias9904@gmail.com>
2022-07-05 17:31:56 -0500

    menu_responsive_finished

---
+++ js/home.js
@@ -0,0 +1,12 @@
+const abrir = document.querySelector(".hamburguer_menu")
+const cerrar = document.querySelector(".closed_menu")
+
+const menu_flotante = document.querySelector(".menu_left")
+
+abrir.addEventListener("click", function(){
+    menu_flotante.classList.toggle("active")
+})
+cerrar.addEventListener("click", function(){
+    menu_flotante.classList.toggle("active")
+})
+
