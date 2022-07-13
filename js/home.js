3d23cd0ad43fb592ee57925baad1dda7b065f451
Isaias Eduardo Castañeda Diaz <cisaias9904@gmail.com>
2022-07-12 19:45:31 -0500

    crew first steps
    we can find a base to works with dynamic javascript

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 const abrir = document.querySelector(".hamburguer_menu")
 const cerrar = document.querySelector(".closed_menu")
 
-const menu_flotante = document.querySelector(".menu_left")
+const menu_flotante = document.querySelector(".menu_right")
 
 abrir.addEventListener("click", function(){
     menu_flotante.classList.toggle("active")
